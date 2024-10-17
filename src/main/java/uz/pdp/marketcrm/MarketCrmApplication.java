1372618919fbfa3ae0032853437a742b727b8e5b
Abdulaziz <khudoyqulov.04@gmail.com>
2024-10-17 11:12:24 +0500

    Initial commit

---
+++ src/main/java/uz/pdp/marketcrm/MarketCrmApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.marketcrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketCrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketCrmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/pdp/marketcrm/MarketCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.marketcrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketCrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
